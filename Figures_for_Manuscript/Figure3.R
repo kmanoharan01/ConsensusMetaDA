5ece8f24e0f03196dda1b75db3286040d30fcc8a
kmanoharan01 <31337106+kmanoharan01@users.noreply.github.com>
2025-10-24 11:24:26 +1000

    Update Figure3.R

--- Figures_for_Manuscript/Figure3.R
+++ Figures_for_Manuscript/Figure3.R
@@ -1,1 +1,16 @@
 ################### Figure 3 #####################
+
+
+## emp
+biome_file <- "./emp_deblur_150bp.subset_2k.rare_5000.biom"
+
+sample_table_file <-  "./samples_table_emp.txt"
+
+
+## emp
+emp <- build_OTU_counts(biom = biome_file, sample_table = sample_table_file)
+
+emp <- OTU_plots(emp)
+
+
+# An example vignette is provided (https://github.com/kmanoharan01/ConsensusMetaDA/blob/main/inst/doc/ConsensusMetaDA_Manual.html)
