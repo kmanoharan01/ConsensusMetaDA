1c1aff0aaab7b5c0094a40089e7057d07e12bd4e
kmanoharan01 <31337106+kmanoharan01@users.noreply.github.com>
2025-10-24 11:26:24 +1000

    Update Figure2.R

--- Figures_for_Manuscript/Figure2.R
+++ Figures_for_Manuscript/Figure2.R
@@ -1,3 +1,16 @@
-########### Figure 2 ##############
+
+############### Figure 2: Differential abundance UpSet and Venn Diagram ##################
 
 
+#1 Marine plastics
+biome_file <- "/Users/jd116080/myprojects/Matt/MicrobiomeConsensusDE/MicrobiomeConsensusDE/TestData/Hackathon/Studies/sed_plastic_rosato/sed_plastic_rosato_ASVs_table.biom"
+
+sample_table_file <-  "/Users/jd116080/myprojects/Matt/MicrobiomeConsensusDE/MicrobiomeConsensusDE/TestData/Hackathon/Studies/sed_plastic_rosato/sed_plastic_rosato_metadata.tsv"
+
+## Marine plastics
+sediment <- build_OTU_counts(biom = biome_file, sample_table = sample_table_file)
+
+## Marine plastics Differential abundance
+sediment_DA <- OTUs_multi_DA(sediment)
+
+# An example vignette is provided (https://github.com/kmanoharan01/ConsensusMetaDA/blob/main/inst/doc/ConsensusMetaDA_Manual.html)
